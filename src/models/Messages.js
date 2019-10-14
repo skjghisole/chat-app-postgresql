55e2556fe122043ae36d866bdd507faad18bb04f
Stephen Karl Jeoffrey Hisole <skjghisole@yahoo.com>
2019-10-13 21:36:21 -0400

    Refactored messages model to have association with the user

--- src/models/Messages.js
+++ src/models/Messages.js
@@ -4,18 +4,15 @@ const model = (sequelize, DataTypes) => {
     content: {
       type: DataTypes.STRING,
       allowNull: false
-    },
-    senderId: {
-      type: DataTypes.INTEGER,
-      allowNull: false
     }
   }, {});
 
   Messages.associate = (models) => {
   	Messages.belongsTo(models.Users, {
-  		foreignKey: 'senderId',
-  		onDelete: 'CASCADE'
-  	})
+      foreignKey: 'senderId',
+      soureKey: 'id',
+      onDelete: 'CASCADE'
+    })
   };
   return Messages;
 };
