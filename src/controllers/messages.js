4cbff2353af0cc2652f7b08ee33ee561b6a4da16
Stephen Karl Jeoffrey Hisole <skjghisole@yahoo.com>
2019-10-17 21:29:21 -0400

    updated the query to include the model assiociated to it

--- src/controllers/messages.js
+++ src/controllers/messages.js
@@ -1,11 +1,13 @@
 import models from '../models';
-const { Messages } = models
+import server from '../'
+const { Messages, Users } = models
 
 const controller = {
 	create: async (req, res) => {
 		const { content, senderId } = req.body
 		try {
 			const newMessage = await Messages.create({ content, senderId: ~~senderId })
+			req.socket.emit('newMsg', newMessage)
 			res.status(201).send(newMessage)
 		} catch (err) {
 			res.status(400).send(err)
@@ -15,7 +17,10 @@ const controller = {
 		const query = req.body
 		try {
 			const messages = await Messages.findAll({
-				where: {...query}
+				where: {...query},
+				include: [{
+					model: Users
+				}]
 			})
 			res.status(200).send(messages)
 		} catch (err) {
