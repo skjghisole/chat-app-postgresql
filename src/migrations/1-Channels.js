7954f2a98c23bac303e9fb871dc3b3e1bce461bd
Stephen Karl Jeoffrey Hisole <skjghisole@yahoo.com>
2019-10-19 19:59:00 -0400

    added migrations for channels

---
+++ src/migrations/1-Channels.js
@@ -0,0 +1,27 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Channels', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.STRING
+      },
+      participants: {
+        type: Sequelize.ARRAY(Sequelize.STRING),
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Channels');
+  }
+};
